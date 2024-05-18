902db3978bef23770fed6ec87b514d56c90dd00c
Kenton <krm01@users.noreply.github.com>
2024-05-18 07:32:36 -0700

    update rom info

--- rom_config.h
+++ rom_config.h
@@ -1,1 +1,1 @@
-{ recomp::Game::MM, { 0xB9FF77DAD7D4DDCCULL, u8"zelda_ocarina_mq_dbg.z64", "THE LEGEND OF ZELDA" }},
+{ recomp::Game::MM, { 0x628CEE4B44E34EBEULL, u8"zelda_ocarina_mq_dbg.z64", "THE LEGEND OF ZELDA" }},
