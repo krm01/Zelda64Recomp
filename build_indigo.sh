c78b44b4bf4f17e0b9a1a660b705745455cd3909
krm01 <krm01@users.noreply.github.com>
2025-03-08 16:06:12 -0800

    try build again

--- build_indigo.sh
+++ build_indigo.sh
@@ -12,35 +12,36 @@
 # add baserom_original.z64 into lib/indigo
 
 # Build N64Recomp for windows, since it's used in the build process as well
-#rm -r lib/N64Recomp/build
-#cd lib/N64Recomp
-#mkdir -p build
-#cd build
-#cmake.exe ..
-#cmake.exe --build .
-#cd ../../../
+# rm -r lib/N64Recomp/build
+# cd lib/N64Recomp
+# mkdir -p build
+# cd build
+# cmake.exe ..
+# cmake.exe --build .
+# cd ../../../
+
+# chmod +x N64Recomp.exe
+# chmod +x RSPRecomp.exe
+
 
 # Ensure clean
-#rm -r out build RecompiledFuncs RecompiledPatches indigo.toml aspMain.toml njpgdspMain.toml rsp/aspMain.cpp rsp/aspMain.text.bin rsp/njpgdspMain.cpp rsp/njpgdspMain.text.bin
+rm -r out build RecompiledFuncs RecompiledPatches indigo.toml aspMain.toml njpgdspMain.toml rsp/aspMain.cpp rsp/aspMain.text.bin rsp/njpgdspMain.cpp rsp/njpgdspMain.text.bin
 
 # Produce an N64 ROM to recompile
-#make -C lib/indigo distclean $1
-#make -C lib/indigo init $1 RECOMP=1
-#make -C lib/indigo psetup $1 RECOMP=1
-#make -C lib/indigo release $1 RECOMP=1
+# build indigo with `make release RECOMP=1` and then run copy-to-recomp.sh from indigo repo
+
+# activate the python venv
+. .venv/bin/activate
 
 # Generate recomp configuration from ELF file
 python3 gen_recomp.py lib/indigo/zelda_ocarina_mq_dbg.elf
 
-chmod +x ./lib/N64Recomp/build/Debug/N64Recomp.exe
-chmod +x ./lib/N64Recomp/build/Debug/RSPRecomp.exe
-
 # Run N64Recomp on the config
-./lib/N64Recomp/build/Debug/N64Recomp.exe indigo.toml
+./N64Recomp.exe indigo.toml
 
 # Run RSPRecomp on the ucode configs
-./lib/N64Recomp/build/Debug/RSPRecomp.exe aspMain.toml
-./lib/N64Recomp/build/Debug/RSPRecomp.exe njpgdspMain.toml
+./RSPRecomp.exe aspMain.toml
+./RSPRecomp.exe njpgdspMain.toml
 
 # TODO make this actually work
 # mkdir -p build
--- rom_config.h
+++ rom_config.h
@@ -1,1 +1,1 @@
-{ recomp::Game::MM, { 0x36BA49436A320649ULL, u8"zelda_ocarina_mq_dbg.z64", "THE LEGEND OF ZELDA" }},
+{ recomp::Game::MM, { 0x8EB31EB68ECAEF5CULL, u8"zelda_ocarina_mq_dbg.z64", "THE LEGEND OF ZELDA" }},
